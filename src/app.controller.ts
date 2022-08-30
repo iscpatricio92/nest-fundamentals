d438ecb9b7e10f0f7d368dff7ea2c14c6fc45515
Patricio Gomez <isc.patricio@gmail.com>
2022-08-30 03:42:49 -0500

    controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,38 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('new')
+  getNewEnpoint(): string {
+    return "i'm new'";
+  }
+
+  @Get('/route/')
+  getSlash(): string {
+    return 'con /sas/';
+  }
+  @Get('/products/filter')
+  getProductFilter() {
+    return `product fitler`;
+  }
+
+  @Get('/products/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return `product ${productId}`;
+  }
+
+  @Get('/categories/:id/products/:productId')
+  getCategory(@Param('id') id: string, @Param('productId') productId: string) {
+    return `category ${id} and products ${productId}`;
+  }
+
+  @Get('/products/')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `product ${limit} and offset ${offset} and brand ${brand}`;
   }
 }
